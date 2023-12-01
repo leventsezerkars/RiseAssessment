45191c50654821dbcc0137e65b3555d20ffcb7d1
Levent Sezer Kars <leventsezer2011@gmail.com>
2023-12-02 01:55:46 +0300

    shared class library

---
+++ Shared/RiseAssessment.Core/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RiseAssessment.Core.Dtos;
+
+namespace RiseAssessment.Core.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
---
+++ Shared/RiseAssessment.Core/Dtos/NoContent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiseAssessment.Core.Dtos
+{
+    public class NoContent
+    {
+    }
+}
