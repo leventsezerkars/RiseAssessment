32a3ad00200c8cfbf5302a0c1e31866ddf06da79
Levent Sezer Kars <leventsezer2011@gmail.com>
2023-12-03 14:03:16 +0300

    gateway microservice init

---
+++ Gateways/RiseAssessment.Gateways.Gateway/Program.cs
@@ -0,0 +1,29 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Configuration.AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToLower()}.json");
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+await app.UseOcelot();
+
+app.Run();
